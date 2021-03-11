708e85d149806d1bc2302263702a574e4d30a0cd
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-11 18:40:18 -0500

    Update npm packages and remove some

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
-const connectDB = require('./utilities/db');
-const Pusher = require('pusher');
+const cors = require("cors");
+const connectDB = require("./utilities/db");
+const conversations = require("./models/conversations");
+const Pusher = require("pusher");
 
 const pusher = new Pusher({
-      appId: "1170571",
-      key: `${process.env.PUSHER_KEY}`,
-      secret: `${process.env.PUSHER_SECRET}`,
-      cluster: "us2",
-      useTLS: true
+  appId: "1170571",
+  key: `${process.env.PUSHER_KEY}`,
+  secret: `${process.env.PUSHER_SECRET}`,
+  cluster: "us2",
+  useTLS: true,
 });
 
 // Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
-    // console.log(req.method + " " + req.path + " - " + req.ip);
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
+  // console.log(req.method + " " + req.path + " - " + req.ip);
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
 });
 
 // Middleware
@@ -29,37 +30,45 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Listen for error events on the database connection
-mongoose.connection.on('error', err => {
-    // Will not log if database disconnects, need to listen for disconnection for that
-    logError(err);
+mongoose.connection.on("error", (err) => {
+  // Will not log if database disconnects, need to listen for disconnection for that
+  logError(err);
 });
 
 // Execute a callback when the connection to mongodb is open
 // because mongodb is not a real-time db we need to implement Pusher
 // Firebase is an example of a real-time db
-mongoose.connection.once('open', () => {
-    console.log('Database Connected');
+mongoose.connection.once("open", () => {
+  console.log("Database Connected");
 
-    // watches everything that happens with the database
-    const changeStream = mongoose.connection.collection('conversations').watch();
-    changeStream.on('change', (change) => {
-        if (change.operationType === 'insert') {
-            pusher.trigger('channels', 'newChannels', {
-                'change': change
-            });
-        } else if (change.operationType === 'update') {
-            pusher.trigger('conversation', 'newMessage', {
-                'change': change
-            });
-        } else {
-            console.log('changeStream error');
-        }
-    });
+  // watches everything that happens with the database
+  const changeStream = mongoose.connection.collection("conversations").watch();
+  changeStream.on("change", (change) => {
+    if (change.operationType === "insert") {
+      pusher.trigger("channels", "newChannels", {
+        change: change,
+      });
+    } else if (change.operationType === "update") {
+      pusher.trigger("conversation", "newMessage", {
+        change: change,
+      });
+    } else {
+      console.log("changeStream error");
+    }
+  });
 });
 
-app.get('/', (req, res) => res.status(200).send('get on route "/" works!');
+app.get("/", (req, res) => res.status(200).send('get on route "/" works!'));
+
+app.post("/new/channel", (req, res) => {
+  const body = req.body;
+
+  conversations.create(body, (err, data) => {
+    err ? res.status(500).send(err) : res.status(201).send(data);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
