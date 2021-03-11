9aba33895937b30f0fd811b85f0ea8bc09191822
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-11 17:34:17 -0500

    Add Pusher to assist in making app real-time

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const connectDB = require('./utilities/db');
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+      appId: "1170571",
+      key: `${process.env.PUSHER_KEY}`,
+      secret: `${process.env.PUSHER_SECRET}`,
+      cluster: "us2",
+      useTLS: true
+});
 
 // Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
@@ -18,6 +27,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
+
+// Listen for error events on the database connection
+mongoose.connection.on('error', err => {
+    // Will not log if database disconnects, need to listen for disconnection for that
+    logError(err);
+});
+
+// Execute a callback when the connection to mongodb is open
+// because mongodb is not a real-time db we need to implement Pusher
+// Firebase is an example of a real-time db
+mongoose.connection.once('open', () => {
+    console.log('Database Connected');
+
+    // watches everything that happens with the database
+    const changeStream = mongoose.connection.collection('conversation').watch();
+    changeStream.on('change', (change) => {
+        if (change.operationType === 'insert') {
+            pusher.trigger('channels', 'newChannels', {
+                'change': change
+            });
+        } else if (change.operationType === 'update') {
+            pusher.trigger('conversation', 'newMessage', {
+                'change': change
+            });
+        } else {
+            console.log('changeStream error');
+        }
+    });
+});
 
 app.get('/', (req, res) => res.status(200).send('get on route "/" works!');
 
