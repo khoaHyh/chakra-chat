9d47234426ad7044a84aafbf3adfbce6dd4be46c
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-13 11:01:07 -0500

    Migrate app to use create-react-app instead of Next.js

--- app-frontend/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import App from "../components/App";
-
-const Home = () => {
-  return <App />;
-};
-
-export default Home;
