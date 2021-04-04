57916d0a98db900313f2f12faffff18e543b6a1e
khoaHyh <khoahuynh.toronto@gmail.com>
2021-04-03 22:47:06 -0400

    Add simple socket emit for message

--- src/components/Routes/Chat/Chat.js
+++ src/components/Routes/Chat/Chat.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { Box, Flex, Heading, Button } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { Box, Flex, Heading, Button, Input } from '@chakra-ui/react';
 import { useHistory } from 'react-router-dom';
 import io from 'socket.io-client';
 import { handleLogout } from '../../Authentication/AuthUtils';
 
 const SERVER = 'http://localhost:3080';
 //const SERVER = 'https://discord-clone-api-khoahyh.herokuapp.com/';
-const socket = io(SERVER, {
-  withCredentials: true,
-});
+
+let socket;
 
 export const Chat = () => {
+  const [input, setInput] = useState('');
+  const [messageList, setMessageList] = useState('');
+  const [message, setMessage] = useState('');
+
+  const user = localStorage.getItem('session.id');
+
+  useEffect(() => {
+    socket = io(SERVER, {
+      withCredentials: true,
+    });
+  }, [SERVER]);
+
+  const sendMessage = event => {
+    event.preventDefault();
+
+    let messageContent = {
+      sender: user,
+      message: input,
+    };
+
+    socket.emit('chat message', messageContent);
+    setInput('');
+  };
+
   let history = useHistory();
   const logout = () => {
     handleLogout(() => {
@@ -20,6 +43,17 @@ export const Chat = () => {
 
   return (
     <Box fontSize="xl">
+      <Box>{messageList}</Box>
+      <form>
+        <Input
+          w="80vw"
+          type="text"
+          value={input}
+          placeholder="Enter Message"
+          onChange={event => setInput(event.target.value)}
+        />
+        <Button onClick={sendMessage}>Send Message</Button>
+      </form>
       <Flex w="full" justifyContent="center" alignItems="center" p={5}>
         <Heading>Authenticated!</Heading>
         <Button w={100} m={2} onClick={logout}>
